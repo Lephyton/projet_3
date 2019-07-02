679b063e8ab337a179d4d41e9dd4491322a4cf3b
Lepython <passywebmaster@gmail.com>
2019-07-02 17:35:20 +0100

    modification

--- move.py
+++ move.py
@@ -9,3 +9,19 @@ for ligne in lab:
         print(ligne)
   
     n_ligne = n_ligne + 1
+
+
+
+
+    def verification_deplacement(lab, pos_col, pos_ligne):
+        
+        n_cols = len(data[0])
+        n_lignes = len(data)
+
+        if pos_ligne < 0 or pos_col < 0 or pos_ligne > (n_lignes -1) or pos_col > (n_cols -1) :
+            return None
+
+        elif data[pos_ligne][pos_col] != " " :
+            return None
+        else :
+        return [pos_col, pos_ligne]
--- play.py
+++ play.py
@@ -14,28 +14,24 @@ def read_lab ():
         data[i] = list(data[i].strip())
     return data 
 
-def show_lab (data, pos_perso):
+def show_lab (data, perso, pos_perso):
     for line in data :
         for element in line :
             print(element, end="")
         print()
-    perso = "X"
-    pos_perso = [1, 1]
     n_ligne = 0
-    for ligne in data:
-        if pos_perso[1] == n_ligne :
-            
-            print(ligne, "<- ligne du personnage")
+    for ligne in data :
+        if n_ligne == pos_perso[1]:
+            print(ligne[0:pos_perso[0]] + perso + ligne[pos_perso[0]+1:])
         else :
             print(ligne)
-    
-        n_ligne = n_ligne + 1
+        n_ligne = n_ligne +1
 #Pour positionner notre personnage dans le labyrinthe, nous allons numéroter chaque 
 # case en fonction de sa colonne et de sa ligne.
 
 
 
 data = read_lab()
-show_lab (data)
+show_lab (data, "X", [1, 1])
 
 
