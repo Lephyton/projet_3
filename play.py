be46abce62139ce57af96cf55810f79adcaa0f8d
Lepython <passywebmaster@gmail.com>
2019-07-09 15:31:08 +0100

    modification de la fonction deplacement_perso

--- play.py
+++ play.py
@@ -1,7 +1,4 @@
 
-
-
-
 def read_lab ():
     """
     this function loads 
@@ -30,10 +27,10 @@ def verification_deplacement(data, pos_col, pos_ligne):
     elif data[pos_ligne][pos_col] != " " :
         return False
     else :
-         return True
+        return True
 
 def choix_du_joueur ():
-    choix = input("Entrer votre choix : H = haut, B = bas, G = gauche, J = droite")
+    choix = input("Entrer votre choix : H = haut, B = bas, G = gauche, J = droite  ")
     while len(choix)!= 1 or choix not in ["H", "B", "G", "J"] :
         print ("Vous n'avez pas fait un bon choix, faite un choix ")
         choix = input("Entrer à nouveau votre choix : H = haut, B = bas, G = gauche, J = droite")
@@ -51,7 +48,7 @@ def deplacement_perso (data, pos_perso, choix):
         dep = verification_deplacement(data, pos_perso[0] +1, pos_perso[1])
     if dep == False:
         print("déplacement impossible")
-    print(dep)
+    print(input("Entrer votre choix : H = haut, B = bas, G = gauche, J = droite  "))
 
 
 
