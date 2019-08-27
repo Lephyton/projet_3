cfc9917b8f299284bfce6799817d438c7fe9eca6
Lepython <passywebmaster@gmail.com>
2019-08-27 22:05:10 +0100

    mise à jour du fichier play.py

--- play.py
+++ play.py
@@ -17,48 +17,49 @@ from pygame.locals import *
 from classes import *
 from constants import *
 
-pygame.init()
+def main ():
+    pygame.init()
 
-window = pygame.display.set_mode((size_window, size_window))
-icon = pygame.image.load(image_icon)
-pygame.display.set_icon(icon)
-pygame.display.set_caption(title)
+    window = pygame.display.set_mode((size_window, size_window))
+    icon = pygame.image.load(image_icon)
+    pygame.display.set_icon(icon)
+    pygame.display.set_caption(title)
 
-level = Level()
-level.read_lab()
-level.show_elemt(window)
-order = Order()
-level.data[0][0] = "X"
-pos_perso = [0,0]
+    level = Level()
+    level.read_lab()
+    level.show_elemt(window)
+    order = Order()
+    level.data[0][0] = "X"
+    pos_perso = [0,0]
 
-continuer = True
-while continuer :
-    level.show_lab (window)
-    choix = " "
-    event = pygame.event.wait()
-    print(event)
-    if event.type == QUIT :
-        continuer = False  
-    if not event.type == KEYDOWN:
-        continue
-    if event.key == K_RIGHT:
-        choix = "J"
-    if event.key == K_LEFT:
-        choix = "G"
-    if event.key == K_UP:
-        choix = "H"
-    if event.key == K_DOWN:
-        choix = "B"
-    print(choix)
-
-    new_pos = order.order_move(level.data, pos_perso, choice)
-    if order.game_over(level.data, new_pos[1], new_pos[0]):
-        continuer = False
-    level.data[pos_perso[1]][pos_perso[0]] = " "
-    level.data[new_pos[1]][new_pos[0]] = "X"
-    pos_perso = new_pos
-    print(order.num_ob)
-
+    continuer = True
+    while continuer :
+        level.show_lab (window)
+        choix = " "
+        event = pygame.event.wait()
+        print(event)
+        if event.type == QUIT :
+            continuer = False  
+        if not event.type == KEYDOWN:
+            continue
+        if event.key == K_RIGHT:
+            choix = "J"
+        if event.key == K_LEFT:
+            choix = "G"
+        if event.key == K_UP:
+            choix = "H"
+        if event.key == K_DOWN:
+            choix = "B"
+        print(choix)
+        new_pos = order.order_move(level.data, pos_perso, choice)
+        if order.game_over(level.data, new_pos[1], new_pos[0]):
+            continuer = False
+        level.data[pos_perso[1]][pos_perso[0]] = " "
+        level.data[new_pos[1]][new_pos[0]] = "X"
+        pos_perso = new_pos
+        print(order.num_ob)
+if __name__ == "__main__":
+    main()
    
    
 
